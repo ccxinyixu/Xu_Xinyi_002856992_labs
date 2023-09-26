49ae707158e232de21f78d05649d17e36627cc66
Xinyi Xu <xxu@trincoll.edu>
2023-09-26 00:38:46 -0400

    upload lab_2

---
+++ Lab_2/src/model/VitalSigns.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author xinyixu
+ */
+public class VitalSigns {
+    
+    private double temperature; // decimals
+    private double bloodPressure;
+    private int puls;
+    private String date;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public int getPuls() {
+        return puls;
+    }
+
+    public void setPuls(int puls) {
+        this.puls = puls;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    
+    @Override
+    public String toString() {
+        return date;
+    }
+    
+}
